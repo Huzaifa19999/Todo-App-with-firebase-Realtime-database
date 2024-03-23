90017061f8344067cfd7d352545baa0a920db3ed
Huzaifa Abdul Qadir <90086432+Huzaifa19999@users.noreply.github.com>
2024-03-23 16:31:44 +0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,105 +1,127 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { set, ref, push, getDatabase, onValue , remove , update } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC5wxSRlotWSx5tD328AuqpUCurrXoYrwU",
-    authDomain: "todoapp-9c3b9.firebaseapp.com",
-    databaseURL: "https://todoapp-9c3b9-default-rtdb.firebaseio.com",
-    projectId: "todoapp-9c3b9",
-    storageBucket: "todoapp-9c3b9.appspot.com",
-    messagingSenderId: "759488151700",
-    appId: "1:759488151700:web:47426ee6e8c739a3487481"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  var database = firebase.database();
+  apiKey: "AIzaSyC5wxSRlotWSx5tD328AuqpUCurrXoYrwU",
+  authDomain: "todoapp-9c3b9.firebaseapp.com",
+  databaseURL: "https://todoapp-9c3b9-default-rtdb.firebaseio.com",
+  projectId: "todoapp-9c3b9",
+  storageBucket: "todoapp-9c3b9.appspot.com",
+  messagingSenderId: "759488151700",
+  appId: "1:759488151700:web:47426ee6e8c739a3487481"
+};
 
-
-  function addTodo() {
-    var inputTodo = document.getElementById("inputTodo");
-    var todoText = inputTodo.value.trim(); 
-    if (todoText !== "") {
-        var liElement = document.createElement("li");
-        var liText = document.createTextNode(todoText);
-        liElement.appendChild(liText);
-
-        var delBtn = document.createElement("button");
-        delBtn.textContent = "Delete";
-        delBtn.addEventListener("click", function() {
-            liElement.remove();
-            database.ref('todos/' + liText).remove();
-        });
-        liElement.appendChild(delBtn);
-
-        var editBtn = document.createElement("button");
-        editBtn.textContent = "Edit"; 
-        editBtn.addEventListener("click", function() {
-            console.log("Edit button clicked");
-        });
-        liElement.appendChild(editBtn);
-
-        var list = document.getElementById("todoList");
-        list.appendChild(liElement);
-
-        database.ref('todos/' + todoText).set({
-            text: todoText
-        });
-
-        inputTodo.value = "";
-    } else {
-        alert("Please enter a todo.");
-    }
-}
-
-function renderTodos() {
-    var list = document.getElementById("todoList");
-
-    list.innerHTML = "";
-
-    database.ref('todos').once('value', function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            var todoKey = childSnapshot.key;
-            var todoData = childSnapshot.val();
-            var todoText = todoData.text;
-
-            var liElement = document.createElement("li");
-            var liText = document.createTextNode(todoText);
-            liElement.appendChild(liText);
-
-            var delBtn = document.createElement("button");
-            delBtn.textContent = "Delete";
-            delBtn.addEventListener("click", function() {
-                liElement.remove();
-                database.ref('todos/' + todoKey).remove();
-            });
-            liElement.appendChild(delBtn);
-
-            var editBtn = document.createElement("button");
-            editBtn.textContent = "Edit"; 
-            editBtn.addEventListener("click", function() {
-                console.log("Edit button clicked");
-            });
-            liElement.appendChild(editBtn);
-
-            list.appendChild(liElement);
-        });
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+console.log(database);
+console.log(app);
+window.Addbtn = function () {
+  let input = document.getElementById('todoInput').value;
+  let inputobj = {
+    input: input
+  }
+  document.getElementById('todoInput').value = "";
+  let refn = push(ref(database, "Todo List"));
+  let iteamkey = refn.key;
+  set(ref(database, `Todo List/${iteamkey}`), inputobj)
+    .then(function () {
+      alert('Data Successfully');
+    })
+    .catch(function (err) {
+      alert('Error: ' + err);
     });
+} 
+let di = document.getElementById("todoList");
+if (di.textContent.trim() === "") {
+  // di.style.display = "none";
+  
 }
 
-renderTodos();
 
+window.getdata = function () {
+  let todoref = ref(database, "Todo List");
 
+  onValue(todoref, (dataall) => {
+    let data = dataall.val();
+    let displayElement = document.getElementById("todoList");
 
+    displayElement.innerHTML = " ";
+    for (let key in data) {
+      if (data.hasOwnProperty(key)) {
+        let item = data[key];
+        
+        // div create 
+        let div = document.createElement("div");
+        div.setAttribute("class", "paradiv p-3 pb-0 mt-3 m-2");
+        
+        displayElement.appendChild(div);
+        let p = document.createElement("li");
+        p.setAttribute('class' , 'fs-5 fw-bold')
+        p.textContent = item.input;
 
+        div.appendChild(p);
+        
+        // Delete Btn Strat
+        let delbtn = document.createElement("button");
+        let delbtntext = document.createTextNode('Delete')
+        delbtn.appendChild(delbtntext);
+        delbtn.setAttribute('class','btn btn-danger ps-4 pe-4 ms-5');
+        p.appendChild(delbtn);
+        delbtn.addEventListener('click', function () {
+          // Remove the item from the database
+          remove(ref(database, `Todo List/${key}`))
+          .then(function (){
+          div.remove();
+          alert('Data deleted successfully')
+        })
+        .catch(function (err) {
+          alert('Data Delete failed: ' + err.message)
+        });
+      }); 
 
+      // Edit btn Start
+      let editbtn = document.createElement('button');
+      let editbtntext = document.createTextNode('Edit');
+      editbtn.appendChild(editbtntext);
+      editbtn.setAttribute('class' , 'btn btn-danger ps-4 pe-4 ms-1');
+      editbtn.addEventListener('click' , function (){
+        let promp = prompt('Enter Update Value');
+      if(promp !== null && promp !== item.value){
+      // update(ref(database, `Todo List/${key}`), { input: promptValue })
+      // update(ref(database, `Todo list/${ey}`), { input: promp })
+      update(ref(database, `Todo List/${key}`), { input: promp })
+      .then(function(){
+       p.textContent = promp;
+       alert('Updated successfully');
+      })
+      .catch(function(err){
+        alert('Updated failed: ' + err);
+      })
+      }
+    });
+    p.appendChild(editbtn);
 
-
-
-function deleteAllTodo() {
-    var list = document.getElementById("todoList");
-    list.innerHTML = "";
-
-    database.ref('todos').remove();
+      }
+      
+    }
+    
+  })
 }
+getdata();
 
+window.deleteAll = function () {
+   let refdel = ref(database , 'Todo List');
+   remove(refdel)
+   .then(function () {
+    alert('All Data deleted successfully');
+   })
+   .catch(function (err) {
+      alert('All Data deleted failed ' , err);
+   })
+}
 
 
 
